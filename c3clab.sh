3860ba7b6d0dee4ffa87e584349bd3579d781e34
Tran Anh <79737053+TranAnhOffical@users.noreply.github.com>
2021-03-27 21:16:24 +0700

    Create c3clab.sh

---
+++ c3clab.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Weicome To C3CLab Install Tools"
+echo "--------Instal Update----------"
+apt update
+echo "---------Instal Sudo-----------"
+apt install sudo
+echo "----------Instal Git------------"
+sudo apt install git
+echo "--------Instal Curl----------"
+sudo apt install curl
+echo "--------Instal Nodejs----------"
+curl -sL
+https://deb.nodesource.com/setup_12.x
+| sudo -E bash -
+echo "--------Instal Nodejs 2----------"
+sudo apt install nodejs
+echo "--------Instal C3C----------"
+git clone https://github.com/c3cbot/c3c-0x
+cd c3c-0x
+npm i
+echo "You Can Using Nano GUI To edit config.json & add fbstate"
+echo "Start bot : npm start"
