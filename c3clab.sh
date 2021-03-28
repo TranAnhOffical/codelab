5a6fbec6d8d677b8c71cfc614a19931df80a6829
Tran Anh <79737053+TranAnhOffical@users.noreply.github.com>
2021-03-28 17:05:50 +0700

    Update c3clab.sh

--- c3clab.sh
+++ c3clab.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
-echo "Weicome To C3CLab Install Tools"
-echo "--------Instal Update----------"
+echo "Chào Mừng Bạn Đến Với Công Cụ Cài Đặt C3CBot tự động"
+echo "---- Các bản cập nhập công cụ ----"
+echo "1.0 - Phiên bản đầu tiên ( Hiện tại )"
+echo "2.0 - Sửa lỗi và bổ sung"
+echo "---- Phiên bản hiện tại : 1.0 ----"
+sleep 5
+echo "Tiến hành cài đặt C3CBot"
+sleep 5
+clear
+echo "Cài đặt Cập Nhập"
 apt update
 clear
-echo "---------Instal Sudo-----------"
+echo "Cài đặt Sudo"
 apt install sudo
 clear
-echo "----------Instal Git------------"
-sudo apt install git
-clear
-echo "--------Instal Curl----------"
-sudo apt install curl
-clear
-echo "--------Instal Nodejs----------"
-sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash-
+echo "Cài đặt Curl"
 sudo apt install curl build-essential
 clear
-echo "--------Instal Nodejs 2----------"
+echo "Cài đặt Nodejs"
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+clear
+echo "Cài đặt Nodejs"
 sudo apt install nodejs
 clear
-echo "--------Instal C3C----------"
+echo "Cài đặt GIT"
+sudo apt install git
+clear
+echo "Clone C3C Bot"
 git clone https://github.com/c3cbot/c3c-0x
+clear
+echo "Cài đặt C3CBot"
 cd c3c-0x
+clear
+echo "Cài đặt NPM"
 npm i
 clear
-echo "You Can Using Nano GUI To edit config.json & add fbstate"
-echo "Start bot : npm start"
+echo "Để bật bot sử dụng lệnh 'npm start' nhé"
+echo "Sử dụng Nano GUI để chỉnh config.json nhé"
+echo "Nhớ Add FBSTATE cho bot nhé"
+sleep 15
